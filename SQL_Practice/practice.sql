53d4a760af6446032c04c489cd5b43d5aa89c787
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-09-14 16:16:37 +0530

    new file added

---
+++ SQL_Practice/practice.sql
@@ -0,0 +1,61 @@
+-- Active: 1788699480576@@127.0.0.1@5432@postgres
+
+
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2)
+);
+
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department, Salary) VALUES
+(1, 'John', 'Doe', 'HR', 55000.00),
+(2, 'Jane', 'Smith', 'IT', 75000.00),
+(3, 'Emily', 'Jones', 'Finance', 65000.00),
+(4, 'Michael', 'Brown', 'IT', 80000.00),
+(5, 'Sarah', 'Davis', 'HR', 60000.00),
+(6, 'David', 'Wilson', 'Finance', 70000.00),
+(7, 'Laura', 'Garcia', 'IT', 72000.00),
+(8, 'Robert', 'Miller', 'HR', 58000.00),
+(9, 'Sophia', 'Martinez', 'Finance', 67000.00),
+(10, 'James', 'Anderson', 'IT', 81000.00);
+
+
+
+--1) How do you select all columns from the Employees table?
+select * From Employees;
+
+
+--2) How do you select only the FirstName and Last Name columns from the Employees table?
+select FirstName ,LastName from Employees;
+
+
+--3) How do you find all employees who work in the 'IT' department?
+select * from Employees where department='IT';
+
+
+
+
+--4) How do you select employees with a salary greater than 70,000?
+SELECT * from Employees where salary>70000;
+
+
+--5) How do you sort the results by Last Name in ascending order?
+select * from Employees order by Lastname asc;
+
+
+--6) How do you select distinct departments from the Employees table?
+select DISTINCT department from Employees 
+
+--7) How do you count the number of employees in each department?
+select department, count(department) as total_ from Employees GROUP BY department;
+
+--8) How do you find the maximum salary in the Employees table?
+select max(salary) as maximum_salary from Employees;
+--9) How do you find the average salary of employees in the 'Finance' department?
+select department , avg(salary) as avg_salary from Employees where department='Finance' GROUP BY department ;
+
+--10) How do you select employees whose last name starts with 'M'?
+select  from Employees where Lastname like 'M%'
